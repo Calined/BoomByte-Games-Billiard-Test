3a57fe2b646daf0722d7b7e62e2059ea188df6a0
Calined <calined@gmx.de>
2020-11-21 15:57:16 +0100

    added push method and made scene loaded

---
+++ Assets/Scripts/PushOnCharge.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushOnCharge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            PushBall();
+        }
+    }
+
+
+    void PushBall()
+    {
+        Debug.Log("Push");
+    }
+
+
+}
