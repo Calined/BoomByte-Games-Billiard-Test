c2f620b615d5d010f3c16c0872a6ca6aa1e850d2
Calined <calined@gmx.de>
2020-11-25 08:47:44 +0100

    balls are now playing collision sound

---
+++ Assets/Scripts/BallCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        myAudioSource.Play();
+
+    }
+
+}
